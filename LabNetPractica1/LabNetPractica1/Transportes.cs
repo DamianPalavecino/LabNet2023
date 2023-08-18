7dcc590a94443f769e30655d6bafe87b7c9a7ce5
Damian Palavecino <damianpalavecino25@gmail.com>
2023-08-18 20:41:58 -0300

    Se crearon las clases Omnibus y Taxi

--- LabNetPractica1/LabNetPractica1/Transportes.cs
+++ LabNetPractica1/LabNetPractica1/Transportes.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
 namespace LabNetPractica1
 {
     public abstract class TransportePublico
     {
-        private int cantidadPasajeros;
+        protected int cantidadPasajeros;
 
         public TransportePublico(int cantidadPasajeros)
         {
@@ -17,5 +14,60 @@ namespace LabNetPractica1
         public abstract void Avanzar();
         public abstract void Detenerse();
     }
+
+    public class Omnibus : TransportePublico
+    {
+        private int pasajeros;
+
+        public Omnibus(int pasajeros) : base (cantidadPasajeros)
+        {
+            this.pasajeros = pasajeros;
+        }
+
+        public override string Avanzar()
+        {
+            return "El Omnibus esta avanzando.";
+        }
+
+        public override string Detenerse()
+        {
+            return "El Omnibus se detuvo.";
+        }
+
+        public int PasajerosOmnibus
+        {
+            get { return pasajeros; }
+            set { pasajeros = value; }
+        }
+
+    }
+
+    public class Taxi : TransportePublico
+    {
+        private int pasajeros;
+
+        public Taxi(int pasajeros) : base(cantidadPasajeros)
+        {
+            this.pasajeros = pasajeros;
+        }
+
+        public override string Avanzar()
+        {
+            return "El Taxi esta avanzando.";
+        }
+
+        public override string Detenerse()
+        {
+            return "El Taxi se detuvo.";
+        }
+
+        public int PasajerosTaxi
+        {
+            get { return pasajeros; }
+            set { pasajeros = value; }
+        }
+
+    }
+
 }
 
