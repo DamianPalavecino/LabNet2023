19fa022438713d1590ca57660c7a2242ecd3dd1a
Damian Palavecino <damianpalavecino25@gmail.com>
2023-08-18 20:14:32 -0300

    Se crearon los archivos para la segunda practica

---
+++ LabNetPractica1/LabNetPractica1/Transportes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace LabNetPractica1
+{
+    public abstract class TransportePublico
+    {
+        private int cantidadPasajeros;
+
+        public TransportePublico(int cantidadPasajeros)
+        {
+            this.cantidadPasajeros = cantidadPasajeros;
+        }
+
+        public abstract void Avanzar();
+        public abstract void Detenerse();
+    }
+}
+
