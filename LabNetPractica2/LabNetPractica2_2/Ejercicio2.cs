65328ca5db5c4e98c139e420cafcd4d503866bcd
Damian Palavecino <damianpalavecino25@gmail.com>
2023-08-22 22:50:29 -0300

    Se creo el metodo correspondiente al Ejercicio 2

---
+++ LabNetPractica2/LabNetPractica2_2/Ejercicio2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNetPractica2_2
+{
+    public static class Ejercicio2
+    {
+        public static void DividirPor(this int dividendo, int divisor) {
+            try
+            {
+                int resultado = dividendo / divisor;
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+            catch (DivideByZeroException excepcion)
+            {
+                Console.WriteLine("¡Solo Chuck Norris divide por cero!");
+                Console.WriteLine($"Excepción: {excepcion.Message}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("¡Seguro ingresó una letra o no ingresó nada!");
+            }
+        }
+    }
+}
