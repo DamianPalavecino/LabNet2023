22bf934e600f6cff8767d727424b811d76b73dc8
Damian Palavecino <damianpalavecino25@gmail.com>
2023-09-13 22:00:47 -0300

    Se creo la conexión a la API pública

---
+++ LabNetPractica3/Lab.EF.MVC/Controllers/TodosController.cs
@@ -0,0 +1,36 @@
+﻿using Lab.EF.MVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace Lab.EF.MVC.Controllers
+{
+    public class TodosController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                var response = httpClient.GetAsync("todos").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    List<TodosView> todosList = JsonConvert.DeserializeObject<List<TodosView>>(result);
+                    return View("Index", todosList);
+                }
+                else
+                {
+                    return View("Error");
+                }
+
+            } catch (Exception) {
+                throw;
+            }
+        }
+    }
+}
